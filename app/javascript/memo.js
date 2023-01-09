ca0287c063855cd12d2ddf9abf20ef4ff6c61a3a
watarunakade <wataru092783@gmail.com>
2023-01-09 15:25:56 +0900

    修正4

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,16 +1,3 @@
-function post (XHR){
-  const item = XHR.response.post;
-  const html = `
-    <div class="post">
-      <div class="post-date">
-        投稿日時:${item.created_at}
-      </div>
-      <div class="post-content">
-        ${item.content}
-      </div>
-    </div>`;
-  return html;
-};
 
 function post (){
   const submit = document.getElementById("submit");
